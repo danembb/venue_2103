efadc180afc1f6ed39c188050c94549fa3134fc0
Dane Brophy <heartbeat@Danes-MacBook-Pro.local>
2021-03-24 16:33:06 -0600

    Initial Commit

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,11 @@
 class Venue
+#  attr_reader :name
+
+#  def initialize (name, capacity)
+#    @name = name
+#    @capacity = capacity
+#  end
+
+#  venue.name = 'Bluebird'
+#  venue.capacity = 4
 end
